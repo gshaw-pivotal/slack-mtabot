3f3e85d494b5778a3ef40acfec71b8fed7f30a3e
gshaw-pivotal <gshaw+github@pivotal.io>
2018-01-24 22:08:27 -0500

    Refactor status controller to include the use of a responder class
    Done in prep for futher development of the status feature.

--- src/controllers/status_controller.js
+++ src/controllers/status_controller.js
@@ -1,14 +1,14 @@
+require('../responders/status_responder.js');
+
 module.exports = function (controller) {
     controller.hears(['status'], 'direct_message,direct_mention,mention', function(bot, message) {
         var messageText = message.text.toLowerCase()
         if (messageText.endsWith('status')) {
-            bot.reply(message,
-                'The current status of all MTA lines are:')
+            statusResponse(controller, bot, message, 'The current status of all MTA lines are:')
         }
         else {
             var line = messageText.split('status')[1].trim().split('\\s+|,\\s?')
-            bot.reply(message,
-                'The current status of ' + line[0].toUpperCase() + ' trains is:')
+            statusResponse(controller, bot, message, 'The current status of ' + line[0].toUpperCase() + ' trains is:')
         }
     })
 }
---
+++ src/responders/status_responder.js
@@ -0,0 +1,5 @@
+statusResponse = function (controller, bot, causeMessage, resultMessage){
+    controller.storage.users.get(causeMessage.user,function(err, user) {
+        bot.reply(causeMessage, resultMessage);
+    });
+};
