540d88c5c3ea29c01917d3eab6ae7d5ba662b3ed
gshaw-pivotal <gshaw+github@pivotal.io>
2018-01-25 00:37:26 -0500

    Add support for rich formats to status responses
    Start of support to allow status formatter to respond using slack
    attachments.

--- src/formatters/status_formatter.js
+++ src/formatters/status_formatter.js
@@ -5,3 +5,23 @@ formatTextStatusAllLines = function () {
 formatTextStatusSingleLine = function (line) {
     return 'The current status of ' + line.toUpperCase() + ' trains is:'
 }
+
+formatRichStatusAllLines = function () {
+    return {
+        attachments:[
+            {
+                pretext: 'The current status of all MTA lines are:'
+            }
+        ]
+    }
+}
+
+formatRichStatusSingleLine = function (line) {
+    return {
+        attachments:[
+            {
+                pretext: 'The current status of ' + line.toUpperCase() + ' trains is:'
+            }
+        ]
+    }
+}
--- test/formatters/status_formatter.test.js
+++ test/formatters/status_formatter.test.js
@@ -18,4 +18,22 @@ describe('mta status formatter', () => {
             })
         })
     })
+
+    describe('for the rich response', () => {
+        describe('when the status of all lines is requested', () => {
+            it('returns the header text for the status of all lines', () => {
+                expectation = 'The current status of all MTA lines are:'
+
+                expect(formatRichStatusAllLines().attachments[0].pretext).toContain(expectation)
+            })
+        })
+
+        describe('when the status of a single line is requested', () => {
+            it('returns the header text for the single line requested', () => {
+                expectation = 'The current status of N trains is:'
+
+                expect(formatRichStatusSingleLine('n').attachments[0].pretext).toContain(expectation)
+            })
+        })
+    })
 })
