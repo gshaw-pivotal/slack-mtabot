ed44033676d837feae8d35fe253195001996a02c
gshaw-pivotal <gshaw+github@pivotal.io>
2018-01-25 23:32:53 -0500

    Status contoller supports plain text and rich requests
    The controller implements logic that will provide either a plain text
    response or a rich response, using attachments based on the request
    received.

--- src/controllers/status_controller.js
+++ src/controllers/status_controller.js
@@ -2,14 +2,25 @@ require('../formatters/status_formatter')
 require('../responders/status_responder');
 
 module.exports = function (controller) {
-    controller.hears(['status'], 'direct_message,direct_mention,mention', function(bot, message) {
+    controller.hears(['status-text'], 'direct_message,direct_mention,mention', function(bot, message) {
         var messageText = message.text.toLowerCase()
-        if (messageText.endsWith('status')) {
+        if (messageText.endsWith('status-text')) {
             statusResponse(controller, bot, message, formatTextStatusAllLines())
         }
         else {
-            var line = messageText.split('status')[1].trim().split('\\s+|,\\s?')
+            var line = messageText.split('status-text')[1].trim().split('\\s+|,\\s?')
             statusResponse(controller, bot, message, formatTextStatusSingleLine(line[0]))
+        }
+    })
+
+    controller.hears(['status'], 'direct_message,direct_mention,mention', function(bot, message) {
+        var messageText = message.text.toLowerCase()
+        if (messageText.endsWith('status')) {
+            statusResponse(controller, bot, message, formatRichStatusAllLines())
+        }
+        else {
+            var line = messageText.split('status')[1].trim().split('\\s+|,\\s?')
+            statusResponse(controller, bot, message, formatRichStatusSingleLine(line[0]))
         }
     })
 }
--- test/controllers/status_controller.test.js
+++ test/controllers/status_controller.test.js
@@ -9,134 +9,270 @@ describe('status controller test', () => {
         statusController(this.controller)
     })
 
-    describe('upon receiving a direct message ending with status keyword', () => {
-        it('responds with a message indicating the status of all mta subway lines', () => {
-            return this.bot.usersInput(
-                [
-                    {
-                        type: 'direct_message',
-                        user: 'aUserId',
-                        channel: 'aChannel',
-                        messages: [
-                            {
-                                text: 'status',
-                                isAssertion: true
-                            }
-                        ]
-                    }
-                ]
-            ).then((message) => {
-                return expect(message.text).toContain('The current status of all MTA lines are:')
+    describe('for plain text requests', () => {
+        describe('upon receiving a direct message ending with status-text keyword', () => {
+            it('responds with a message indicating the status of all mta subway lines', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_message',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status-text',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.text).toContain('The current status of all MTA lines are:')
+                })
+            })
+        })
+
+        describe('upon receiving a direct mention ending with status-text keyword', () => {
+            it('responds with a message indicating the status of all mta subway lines', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status-text',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.text).toContain('The current status of all MTA lines are:')
+                })
+            })
+        })
+
+        describe('upon receiving a mention ending with status-text keyword', () => {
+            it('responds with a message indicating the status of all mta subway lines', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status-text',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.text).toContain('The current status of all MTA lines are:')
+                })
+            })
+        })
+
+        describe('upon receiving a direct message containing (but not ending with) the status-text keyword', () => {
+            it('responds with a message indicating the status for a line', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_message',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status-text n',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.text).toContain('The current status of N trains is:')
+                })
+            })
+        })
+
+        describe('upon receiving a direct mention containing (but not ending with) the status-text keyword', () => {
+            it('responds with a message indicating the status for a line', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status-text w',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.text).toContain('The current status of W trains is:')
+                })
+            })
+        })
+
+        describe('upon receiving a mention containing (but not ending with) the status-text keyword', () => {
+            it('responds with a message indicating the status for a line', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status-text r',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.text).toContain('The current status of R trains is:')
+                })
             })
         })
     })
 
-    describe('upon receiving a direct mention ending with status keyword', () => {
-        it('responds with a message indicating the status of all mta subway lines', () => {
-            return this.bot.usersInput(
-                [
-                    {
-                        type: 'direct_mention',
-                        user: 'aUserId',
-                        channel: 'aChannel',
-                        messages: [
-                            {
-                                text: 'status',
-                                isAssertion: true
-                            }
-                        ]
-                    }
-                ]
-            ).then((message) => {
-                return expect(message.text).toContain('The current status of all MTA lines are:')
+    describe('for rich text requests', () => {
+        describe('upon receiving a direct message ending with status keyword', () => {
+            it('responds with a message indicating the status of all mta subway lines', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_message',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.attachments[0].pretext).toContain('The current status of all MTA lines are:')
+                })
             })
         })
-    })
 
-    describe('upon receiving a mention ending with status keyword', () => {
-        it('responds with a message indicating the status of all mta subway lines', () => {
-            return this.bot.usersInput(
-                [
-                    {
-                        type: 'mention',
-                        user: 'aUserId',
-                        channel: 'aChannel',
-                        messages: [
-                            {
-                                text: 'status',
-                                isAssertion: true
-                            }
-                        ]
-                    }
-                ]
-            ).then((message) => {
-                return expect(message.text).toContain('The current status of all MTA lines are:')
+        describe('upon receiving a direct mention ending with status keyword', () => {
+            it('responds with a message indicating the status of all mta subway lines', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.attachments[0].pretext).toContain('The current status of all MTA lines are:')
+                })
             })
         })
-    })
 
-    describe('upon receiving a direct message containing (but not ending with) the status keyword', () => {
-        it('responds with a message indicating the status for a line', () => {
-            return this.bot.usersInput(
-                [
-                    {
-                        type: 'direct_message',
-                        user: 'aUserId',
-                        channel: 'aChannel',
-                        messages: [
-                            {
-                                text: 'status n',
-                                isAssertion: true
-                            }
-                        ]
-                    }
-                ]
-            ).then((message) => {
-                return expect(message.text).toContain('The current status of N trains is:')
+        describe('upon receiving a mention ending with status keyword', () => {
+            it('responds with a message indicating the status of all mta subway lines', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.attachments[0].pretext).toContain('The current status of all MTA lines are:')
+                })
             })
         })
-    })
 
-    describe('upon receiving a direct mention containing (but not ending with) the status keyword', () => {
-        it('responds with a message indicating the status for a line', () => {
-            return this.bot.usersInput(
-                [
-                    {
-                        type: 'direct_mention',
-                        user: 'aUserId',
-                        channel: 'aChannel',
-                        messages: [
-                            {
-                                text: 'status w',
-                                isAssertion: true
-                            }
-                        ]
-                    }
-                ]
-            ).then((message) => {
-                return expect(message.text).toContain('The current status of W trains is:')
+        describe('upon receiving a direct message containing (but not ending with) the status keyword', () => {
+            it('responds with a message indicating the status for a line', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_message',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status n',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.attachments[0].pretext).toContain('The current status of N trains is:')
+                })
             })
         })
-    })
 
-    describe('upon receiving a mention containing (but not ending with) the status keyword', () => {
-        it('responds with a message indicating the status for a line', () => {
-            return this.bot.usersInput(
-                [
-                    {
-                        type: 'mention',
-                        user: 'aUserId',
-                        channel: 'aChannel',
-                        messages: [
-                            {
-                                text: 'status r',
-                                isAssertion: true
-                            }
-                        ]
-                    }
-                ]
-            ).then((message) => {
-                return expect(message.text).toContain('The current status of R trains is:')
+        describe('upon receiving a direct mention containing (but not ending with) the status keyword', () => {
+            it('responds with a message indicating the status for a line', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'direct_mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status w',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.attachments[0].pretext).toContain('The current status of W trains is:')
+                })
+            })
+        })
+
+        describe('upon receiving a mention containing (but not ending with) the status keyword', () => {
+            it('responds with a message indicating the status for a line', () => {
+                return this.bot.usersInput(
+                    [
+                        {
+                            type: 'mention',
+                            user: 'aUserId',
+                            channel: 'aChannel',
+                            messages: [
+                                {
+                                    text: 'status r',
+                                    isAssertion: true
+                                }
+                            ]
+                        }
+                    ]
+                ).then((message) => {
+                    return expect(message.attachments[0].pretext).toContain('The current status of R trains is:')
+                })
             })
         })
     })
